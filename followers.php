5ae9e1be928e028f57eb775123669c54c252e552
clara <clara.jacot@gmail.com>
2024-02-06 15:38:53 +0100

    connexion OK

--- followers.php
+++ followers.php
@@ -14,16 +14,16 @@ require 'connexion.php'
             <img src="resoc.jpg" alt="Logo de notre réseau social"/> 
             <nav id="menu">
                 <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
+                <a href="wall.php?user_id=<?php echo $connectedId?>">Mur</a>
+                <a href="feed.php?user_id=<?php echo $connectedId?>">Flux</a>
                 <a href="tags.php?tag_id=1">Mots-clés</a>
             </nav>
             <nav id="user">
                 <a href="#">Profil</a>
                 <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
+                    <li><a href="settings.php?user_id=<?php echo $connectedId?>">Paramètres</a></li>
+                    <li><a href="followers.php?user_id=<?php echo $connectedId?>">Mes suiveurs</a></li>
+                    <li><a href="subscriptions.php?user_id=<?php echo $connectedId?>">Mes abonnements</a></li>
                 </ul>
 
             </nav>
