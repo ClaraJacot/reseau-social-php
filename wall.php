cb89f035098925fe6335039d541db23d543e078f
AdrienCmn <adriencheminant@proton.me>
2024-02-07 15:09:24 +0100

    Etape 5 Niveau 2

--- wall.php
+++ wall.php
@@ -33,6 +33,7 @@ require 'connexion.php'
             
 
             <aside>
+                <p>
                 <?php
                 /**
                  * Etape 3: récupérer le nom de l'utilisateur
@@ -52,6 +53,7 @@ require 'connexion.php'
                         // et complétez le code ci dessous en remplaçant les ???
                         $authorId = $connectedId;
                         $postContent = $_POST['message'];
+                        $namePost = $user['alias'];
 
 
                         //Etape 3 : Petite sécurité
@@ -69,10 +71,30 @@ require 'connexion.php'
                             echo "Impossible d'ajouter le message: " . $mysqli->error;
                         } else
                         {
-                            echo "Message posté en tant que :" [$authorId];
+                            echo "Message posté en tant que " , $namePost;
                         }
                     }
-                ?>
+                
+                $enCoursDeTraitement2 = isset($_POST['button']);
+                if($enCoursDeTraitement2) {
+                    $followed = $userId;
+                    $following = $connectedId;
+                    $nameFollowed = $user['alias'];
+                    $lInstructionSql2 = "INSERT INTO followers (id, followed_user_id, following_user_id)
+                        VALUES (NULL, $followed, $following)";
+                
+                
+                    $ok = $mysqli->query($lInstructionSql2);
+                    if (! $ok)
+                    {
+                        echo "Impossible de suivre cette personne." . $mysqli->error;                 
+                    } else 
+                    {
+                        echo "Vous suivez bien " ,$nameFollowed;
+                    } 
+                }
+                ?> </p>
+
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
                 <section>
                     <h3>Présentation</h3>
@@ -88,7 +110,12 @@ require 'connexion.php'
                         </dl>
                         <input type='submit'>
                     </form>
+                    <?php else:?>
+                        <form action="wall.php?user_id=<?php echo  $userId?>" method="post">
+                        
+                        <input type='submit' name='button'>
                     <?php endif; ?> </p>  
+                    
                 </section>
             </aside>
             <main>
