0842b945049b6a188539e09ee336206d4543db7a
AdrienCmn <adriencheminant@proton.me>
2024-02-13 10:11:46 +0100

    Ajout Nouvel HASHTAG

--- wall.php
+++ wall.php
@@ -35,9 +35,6 @@ require 'connexion.php'
             <aside>
                 <p>
                 <?php
-
-                 
-
                 /**
                  * Etape 3: récupérer le nom de l'utilisateur
                  */                
@@ -59,9 +56,6 @@ require 'connexion.php'
                         $namePost = $user['alias'];
 
                         $postContent = $mysqli->real_escape_string($postContent);
-
-                      
-
 
                         //Etape 3 : Petite sécurité
                         // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
@@ -102,9 +96,12 @@ require 'connexion.php'
                                     echo "Tag enregistré";
                                 }
                             } 
-
-
-                        } if (str_contains($postContent,'#')){
+                            }
+                            if (str_contains($postContent,'#')){
+                                $uneQuestion = "SELECT id FROM posts ORDER BY posts.id DESC LIMIT 1";
+                                $dernierPost = $mysqli->query($uneQuestion);
+                                $ledernierPost = $dernierPost->fetch_assoc();
+                                $leVraiId = $ledernierPost['id'];
                                 $splittedPost = explode(" ",$postContent);
                                 foreach($splittedPost as $word){
                                     if (str_contains($word,'#')){
@@ -112,16 +109,28 @@ require 'connexion.php'
                                     $nouveauHashtag = $result[1];
                                     $hashtagInsert = "INSERT INTO tags (label)
                                         VALUES ('$nouveauHashtag')";
-                                    
                                     $ok = $mysqli->query($hashtagInsert);
                                         if (! $ok){
                                             echo "Impossible d'ajouter un nouvel hashtag" . $mysqli->error;
                                         }else{
                                             echo "Nouveau tag enregistré";
                                         }
+                                    $question2 = "SELECT id FROM tags ORDER BY tags.id DESC LIMIT 1";
+                                    $reponse = $mysqli->query($question2);
+                                    $lederniertag = $reponse->fetch_assoc();
+                                    $levraitag = $lederniertag['id'];
+                                    $posttagsinsert = "INSERT INTO posts_tags (post_id, tag_id)
+                                        VALUES ($leVraiId, $levraitag)";
+                                    $ok2 = $mysqli->query($posttagsinsert);
+                                    if (! $ok2){
+                                        echo "Impossible d'ajouter à posts_tags" . $mysqli->error;
+                                    }else{
+                                        echo "Nouveau post_tag enregistré";
                                     }
                                 }
-                            }   
+                            }
+                        }
+                
                 }
                 $enCoursDeTraitement2 = isset($_POST['button']);
                 if($enCoursDeTraitement2) {
@@ -150,8 +159,6 @@ require 'connexion.php'
                     $lInstructionSql3 = "INSERT INTO likes (id, user_id, post_id)
                         VALUES (NULL, $liker1, $likedPost1)";
                         
-                
-                
                     $ok = $mysqli->query($lInstructionSql3);
                     if (! $ok)
                     {
