2ac8a0885da78ed78e3863b4752c51c68f07f66e
AdrienCmn <adriencheminant@proton.me>
2024-02-13 15:32:00 +0100

    Fin déclinaison 1 étape 3

--- wall.php
+++ wall.php
@@ -200,7 +200,8 @@ require 'connexion.php'
                  */
                 $laQuestionEnSql = "
                     SELECT posts.content, posts.created, posts.id, users.alias as author_name, 
-                    COUNT(likes.id) as like_number, GROUP_CONCAT(DISTINCT tags.label) AS taglist 
+                    COUNT(likes.id) as like_number, GROUP_CONCAT(DISTINCT tags.label) AS taglist,
+                    GROUP_CONCAT(DISTINCT tags.id ORDER BY tags.label) AS tagid
                     FROM posts
                     JOIN users ON  users.id=posts.user_id
                     LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
@@ -252,11 +253,19 @@ require 'connexion.php'
                                 <input type = 'hidden' name='postId' value = "<?php echo $post['id'] ?>">
                                 <button type='submit' name='like'>Aimer</button>
                             </form>
-
-                            <a href="">#<?php echo $post['taglist']?></a>
-
-
-                        
+                            <?php
+                                $splittedTag = explode(",", $post['taglist']);
+                                $splittedId = explode(",", $post['tagid']);
+                        //    echo $post['taglist'];
+                        //    echo $post['tagid'];
+                        //    print_r($splittedTag);
+                        //    print_r($splittedId);
+                                for ($i = 0 ; $i<count($splittedId); $i ++ ):?>
+                                    <a href ="tags.php?tag_id=<?php
+                                    echo $splittedId[$i];
+                                ?>">#<?php echo $splittedTag[$i] ?></a>;
+                                <?php endfor;
+                            ?>       
                         </footer>
                     </article>
                 <?php } ?>
