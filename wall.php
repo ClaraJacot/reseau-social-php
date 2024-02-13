f353a253c712ab42a8f61624df49abbcda629c7b
clara <clara.jacot@gmail.com>
2024-02-13 10:13:17 +0100

    hashtag fini

--- wall.php
+++ wall.php
@@ -104,22 +104,38 @@ require 'connexion.php'
                                 }
                             } 
 
-
-                        } if (str_contains($postContent,'#')){
+                        } 
+                        if (str_contains($postContent,'#')){
+                                $uneQuestion = "SELECT id FROM posts ORDER BY posts.id DESC LIMIT 1";
+                                $dernierPost = $mysqli->query($uneQuestion);
+                                $ledernierPost = $dernierPost->fetch_assoc();
+                                $leVraiId = $ledernierPost['id'];
                                 $splittedPost = explode(" ",$postContent);
                                 foreach($splittedPost as $word){
                                     if (str_contains($word,'#')){
                                     $result = explode('#', $word);
                                     $nouveauHashtag = $result[1];
                                     $hashtagInsert = "INSERT INTO tags (label)
-                                        VALUES ('$nouveauHashtag')";
-                                    
+                                        VALUES ('$nouveauHashtag')";    
+                                    //echo $hashtagInsert;
                                     $ok = $mysqli->query($hashtagInsert);
                                         if (! $ok){
                                             echo "Impossible d'ajouter un nouvel hashtag" . $mysqli->error;
                                         }else{
                                             echo "Nouveau tag enregistré";
                                         }
+                                    $question2 = "SELECT id FROM tags ORDER BY tags.id DESC LIMIT 1";
+                                    $reponse = $mysqli->query($question2);
+                                    $lederniertag = $reponse->fetch_assoc();
+                                    $levraitag = $lederniertag['id'];
+                                    $posttagsinsert = "INSERT INTO posts_tags (post_id, tag_id)
+                                        VALUES ($leVraiId, $levraitag)";
+                                    $ok2 = $mysqli->query($posttagsinsert);
+                                    if (! $ok2){
+                                        echo "Impossible d'ajouter à posts_tags" . $mysqli->error;
+                                    }else{
+                                        echo "Nouveau post_tag enregistré";
+                                    }
                                     }
                                 }
                             }   
